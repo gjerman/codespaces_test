511e19a057367b83d5cd189caa16296562cd78ad
gjerman <gjerman@gmail.com>
2022-05-16 12:56:12 +0000

    added db

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Plate, PlateSet
+
+engine = create_engine('sqlite:///plates.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
 print("Hello World!")
 print("Hello World again!")
+
